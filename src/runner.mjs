a5e0df77e521f2bf9888ce4a9ccdeb6eb142b1fb
Daniel Irvine <daniel@danielirvine.com>
2019-12-07 10:04:41 +0000

    Add initial test runner

---
+++ src/runner.mjs
@@ -0,0 +1,10 @@
+import path from "path";
+
+export const run = async () => {
+  try {
+    await import(path.resolve(process.cwd(), "test/tests.js"));
+  } catch (e) {
+    console.error(e);
+  }
+  console.log("Test run finished");
+};
